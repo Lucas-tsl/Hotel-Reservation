2e501f254ef3e757945ee86df87973478a73fa5f
Lucas Troteseil <troteseil.lucas@gmail.com>
2024-02-01 18:11:43 +0100

    Check Avaiility Form

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Merienda&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body class="bg-light">
@@ -173,40 +174,38 @@
         </div>
     </div>
 
-    <!-- Check Avaibility form -->
-    <div class="container">
+    <!-- Check avaibility form -->
+    <div class="container avaibility-form">
         <div class="row">
             <div class="col-lg-12 bg-white shadow p-4 rounded">
-                <h5>Check Booking Avaibility</h5>
+                <h5 class="mb-4">Check Booking Avaibility</h5>
                 <form>
-                    <div class="row">
-                        <div class="col-lg-3">
+                    <div class="row align-items-end">
+                        <div class="col-lg-3 mb-3">
                             <label class="form-label" style="font-weight: 500;">Check-in</label>
                             <input type="date" class="form-control shadow-none">
                         </div>
-                        <div class="col-lg-3">
+                        <div class="col-lg-3 mb-3">
                             <label class="form-label" style="font-weight: 500;">Check-out</label>
                             <input type="date" class="form-control shadow-none">
                         </div>
-                        <div class="col-lg-3">
+                        <div class="col-lg-3 mb-3">
                             <label class="form-label" style="font-weight: 500;">Adult</label>
                             <select class="form-select shadow-none">
-                                <option selected>Open this select menu</option>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
                                 <option value="3">Three</option>
                             </select>
                         </div>
-                        <div class="col-lg-2">
+                        <div class="col-lg-2 mb-3">
                             <label class="form-label" style="font-weight: 500;">Child</label>
                             <select class="form-select shadow-none">
-                                <option selected>Open this select menu</option>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
                                 <option value="3">Three</option>
                             </select>
                         </div>
-                        <div class="col-lg-1">
+                        <div class="col-lg-1 mb-lg-3 mt-2">
                             <button type="submit" class="btn text-white shadow-none custom-bg">Submit</button>
                         </div>
                     </div>
@@ -214,6 +213,8 @@
             </div>
         </div>
     </div>
+
+    
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
