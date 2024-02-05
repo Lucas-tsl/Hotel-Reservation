d4eb3e6c7aa1cce5c99ee26def1d498ded488721
Lucas Troteseil <troteseil.lucas@gmail.com>
2024-02-06 00:18:01 +0100

    Homepage Design Finish

--- index.php
+++ index.php
@@ -481,7 +481,10 @@
                     </div>
                 </div>
             </div>
-            <div class="swiper-pagination"></div>
+            <div class="swiper-pagination"></div>            
+        </div>
+        <div class="col-lg-12 text-center mt-5">
+            <a href="" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
         </div>
     </div>
 
@@ -528,11 +531,45 @@
         </div>
     </div>
 
+    <!-- Footer -->
 
-    <br><br><br>
-    <br><br><br>
-
+    <div class="container-fluid bg-white mt-5">
+        <div class="row">
+            <div class="col-lg-4 p-4">
+            <h3 class="h-font fw-fold fs-3 mb-2">LT Hotel</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Quis veritatis quisquam quaerat laboriosam placeat molestiae
+                earum ipsam eum nam asperiores.
+            </p>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Links</h5>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
+                <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+            </div>
+            <div class="col-lg-4 p-4">
+                <h5 class="mb-3">Follow us</h5>
+                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                    <i class="bi bi-instagram me-1"></i> Instagram
+                </a>
+                <br>
+                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                    <i class="bi bi-whatsapp me-1"></i> Whatsapp
+                </a>
+                <br>
+                <a href="#" class="d-inline-block text-dark text-decoration-none">
+                    <i class="bi bi-tiktok me-1"></i> Tik Tok
+                </a>
+                <br>  
+            </div>
+        </div>
+    </div>
     
+    <h6 class="text-center bg-dark text-white p-3 m-0"> Design and Deveped by Cript</h6>
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
