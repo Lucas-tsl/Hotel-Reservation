f5101351dac147da92ea7a263996c420f790d90b
Lucas Troteseil <troteseil.lucas@gmail.com>
2024-02-06 15:19:36 +0100

    Facilitites Page

---
+++ facilities.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LT Hotel - FACILITIES</title>  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <?php require('inc/links.php');?>
+    <link rel="stylesheet" href="css/facilities.css">
+    
+</head>
+
+<body class="bg-light"> 
+    
+    <?php require('inc/header.php');?>
+    
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">Our Facilities</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Distinctio molestias nostrum <br> accusamus ea iusto doloremque cupiditate dolore,
+            ex exercitationem hic!
+        </p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi.svg" width="40px" alt="wifi-icon">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Minima veniam harum tempora veritatis quia aperiam qui.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi.svg" width="40px" alt="wifi-icon">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Minima veniam harum tempora veritatis quia aperiam qui.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi.svg" width="40px" alt="wifi-icon">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Minima veniam harum tempora veritatis quia aperiam qui.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi.svg" width="40px" alt="wifi-icon">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Minima veniam harum tempora veritatis quia aperiam qui.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi.svg" width="40px" alt="wifi-icon">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Minima veniam harum tempora veritatis quia aperiam qui.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/features/wifi.svg" width="40px" alt="wifi-icon">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Minima veniam harum tempora veritatis quia aperiam qui.
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+        
+    <?php require('inc/footer.php');?>
+
+    <!-- Scripts -->
+    
+    
+
+</body>
+
+</html>
